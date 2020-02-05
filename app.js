1f1e98d5963c495691e85d14b8fe35909cba5abc
chris-jburke <cjburke04@gmail.com>
2020-02-05 12:28:15 -0800

    mvp req met

--- app.js
+++ app.js
@@ -85,10 +85,10 @@ let checkLegalMove = function(pieceColor, pieceX, pieceY, targetX, targetY) {
 	if(gameBoard[targetX][targetY].spaceColor === "white"){
 		console.log("Cannot Move");
 		return;
-	} else if(pieceColor !== currentTurn) {
+	} /*else if(pieceColor !== currentTurn) {
 		console.log("Not your turn");
 		return;
-	}
+	}*/
 	let colorAccess = gameBoard[pieceX][pieceY].currPiece.color;
 	let kingAccess = gameBoard[pieceX][pieceY].currPiece.isKing;
 	if(colorAccess === "white" && !kingAccess){
@@ -197,11 +197,33 @@ let jumpMoveHandler = function(pieceColor, pieceX, pieceY, targetX, targetY, jum
 			let domRemoveParent = document.querySelectorAll(selectRemove)[0];
 			let domRemoveChild = document.querySelectorAll(selectRemove)[1];
 			domRemoveParent.removeChild(domRemoveChild);
+			updateGraveyard(jumpColor);
 			updateCurrentTurn(pieceColor);
 			updateScore(pieceColor);
 		}
 	}
 }
+
+let updateGraveyard = function(color) {
+	if(color === "white") {
+		let domGraveyard = document.querySelector(".black-graveyard");
+		let domGraveImage = document.createElement("div");
+		domGraveImage.classList.add("grave-image");
+		let domImage = document.createElement("img");
+		domImage.src = "images/white-piece.png";
+		domGraveImage.appendChild(domImage);
+		domGraveyard.appendChild(domGraveImage);
+	} else {
+		let domGraveyard = document.querySelector(".white-graveyard");
+		let domGraveImage = document.createElement("div");
+		domGraveImage.classList.add("grave-image");
+		let domImage = document.createElement("img");
+		domImage.src = "images/black-piece.png";
+		domGraveImage.appendChild(domImage);
+		domGraveyard.appendChild(domGraveImage);
+	}
+}
+
 let checkLegalKingMove = function(pieceColor, pieceX, pieceY, targetX, targetY) {
 	if(targetX === pieceX - 1 && targetY === pieceY - 1){
 		regularMoveHandlerKing(pieceX, pieceY, targetX, targetY);
@@ -260,7 +282,6 @@ let updateScore = function(pieceColor) {
 }
 
 let clickEventHandler = function(e) {
-	checkIfWhiteHasMove();
 	if(e.target.classList[0] === "row-box" && clickCount % 2 === 0){
 		console.log("return");
 		return;
@@ -285,23 +306,363 @@ let clickEventHandler = function(e) {
 		targetLocation = "";
 
 	}
+	let isWhiteMove = checkIfWhiteHasMove();
+	//let isBlackMove = true;
+	let isBlackMove = checkIfBlackHasMove();
+	//let isKingMove = checkIfKingMove();
+	if(!isWhiteMove){
+		let isKingMove = checkIfKingMove("white");
+		if(!isKingMove) {
+			console.log("Black wins");
+			gameOver = true;
+			gameOverHandler("black");
+		}
+	} else if(!isBlackMove){
+		let isKingMove = checkIfKingMove("black");
+		if(!isKingMove){
+			console.log("White wins");
+			gameOver = true;
+			gameOverHandler("white");
+		}
+	}
+	if(gameOver === false){
+		checkGameOver(); 
+	}
 }
 let checkGameOver = function(){
-	if(whiteScore === 12 || blackScore === 12){
+	if(whiteScore === 12) {
 		gameOver = true;
 		console.log("GAME OVER");
+		gameOverHandler("white");
+	} else if(blackScore === 12) {
+		gameOver = true;
+		gameOverHandler("black");
 	}
 }
 
-let checkIfWhiteHasMove = function(){
-	for(let i = 0; i < 8; i++){
-		for(let j = 0; j < 8; j++) {
+let checkIfKingMove = function(pieceColor){
+	console.log("in check king move");
+	let existMove = false;
+	i = 0;
+	while(!existMove && i < 8){
+		let j = 0;
+		while(!existMove && j < 8){
 			if(gameBoard[i][j].currPiece != null){
-				if(gameBoard[i][j].currPiece.color === "white" && !gameBoard[i][j].currPiece.isKing){
-					console.log("white piece not king");
+				if(gameBoard[i][j].currPiece.isKing && gameBoard[i][j].currPiece.color === pieceColor){
+					console.log("current king: i: " + i + " j: " + j);
+					let testX = 0;
+					let testY = 0;
+					if(i+1 < 8) {
+						testX = i+1;
+						if(j+1 < 8){
+							testY = j+1
+							existMove = checkMove(testX, testY);
+						}
+					}
+					if(existMove){
+						console.log("first check success");
+						console.log("i: " + i);
+						console.log("j: " + j);
+						return existMove;
+					}
+					if(i+1 < 8) {
+						testX = i+1;
+						if(j-1 >= 0) {
+							testY = j-1;
+							existMove = checkMove(testX, testY);
+						}
+					}
+					if(existMove){
+						console.log("second check success");
+						return existMove;
+					}
+					if(i-1 >= 0) {
+						testX = i-1;
+						if(j-1 >= 0){
+							testY = j-1;
+							existMove = checkMove(testX, testY);
+						}
+					}
+					existMove = checkMove(testX, testY);
+					if(existMove){
+						console.log("third check success");
+						console.log("i: " + i);
+						console.log("j: " + j);
+						return existMove;
+					}
+					if(i-1 >= 0) {
+						testX = i -1;
+						if(j+1 < 8) {
+							testY = j+1;
+							existMove = checkMove(testX, testY);
+						}
+					}
+					if(existMove){
+						console.log("i: " + i);
+						console.log("j: " + j);
+						console.log("4th check success");
+						return existMove;
+					}
+					let jumpX = 0;
+					let jumpY = 0;
+					if(i-2 >= 0) {
+						testX = i-2;
+						jumpX = testX+1;
+						if(j-2 >= 0) {
+							testY = j-2;
+							jumpY = testY+1;
+							existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+						}
+					}
+					if(existMove){
+						console.log("tarX: " + testX);
+						console.log("tarY: " + testY);
+						return existMove;
+					}
+					if(i-2 >= 0){
+						testX = i-2;
+						jumpX = testX+1;
+						if(j+2 < 8){
+							testY = j+2;
+							jumpY = testY-1;
+							existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+						}
+					}
+					if(existMove){
+						console.log("tarX " + testX);
+						console.log("tarY " + testY);
+						return existMove;
+					}
+					if(i+2 < 8) {
+						testX = i+2;
+						jumpX = testX - 1;
+						if(j+2 < 8) {
+							testY = j+2;
+							jumpY = testY - 1;
+							existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+						}
+					}
+					if(existMove){
+						return existMove;
+						console.log("tarX: " + testX);
+						console.log("tarY: " + testY);
+					}
+					if(i+2 < 8){
+						testX = i+2;
+						jumpX = testX-1;
+						if(j-2 >= 0) {
+							testY = j-2;
+							jumpY = testY+1;
+							existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+						}
+					}
+					if(existMove){
+						console.log("tarX: " + testX);
+						console.log("tarY: " + testY);
+						return existMove;
+					}
 				}
 			}
+			j++;
 		}
+		i++;
+	}
+	return existMove;
+}
+
+let checkIfWhiteHasMove = function(){
+	let existMove = false;
+	let i = 0;
+	while(!existMove && i < 8){
+		let j = 0;
+		while(!existMove && j < 8) {
+			if(gameBoard[i][j].currPiece != null){
+				if(gameBoard[i][j].currPiece.color === "white" && !gameBoard[i][j].currPiece.isKing){
+					let testX = 0;
+					let testY = 0;
+					if(i+1 < 8) {
+						testX = i+1;
+						if(j+1 < 8){
+							testY = j+1
+							existMove = checkMove(testX, testY);
+						}
+					}
+					console.log("first test i: " + i);
+					console.log("first test j: " + j);
+					//existMove = checkMove(testX, testY);
+					if(existMove){
+						console.log("first check success");
+						console.log("testX: " + testX);
+						console.log("testY " + testY);
+						return existMove;
+					}
+					if(i+1 < 8) {
+						testX = i+1;
+						if(j-1 >= 0) {
+							testY = j-1;
+							existMove = checkMove(testX, testY);
+						}
+					}
+					if(existMove){
+						console.log("second check success");
+						return existMove;
+					}
+					let jumpX = 0;
+					let jumpY = 0;
+					if(i+2 < 8) {
+						testX = i+2;
+						jumpX = testX - 1;
+						if(j+2 < 8) {
+							testY = j+2;
+							jumpY = testY - 1;
+							existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+						}
+					}
+					//existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+					console.log("i: " + i);
+					console.log("j: " + j);
+					if(existMove){
+						console.log("tarX: " + testX);
+						console.log("tarY: " + testY);
+						return existMove;
+					}
+					if(i+2 < 8){
+						testX = i+2;
+						jumpX = testX-1;
+						if(j-2 >= 0){
+							testY = j-2;
+							jumpY = testY+1;
+							existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+						}
+					}
+				}
+			}
+			j++;
+		}
+		i++
+	}
+	console.log(existMove);
+	return existMove;
+}
+
+let checkIfBlackHasMove = function(){
+	let existMove = false;
+	let i = 0;
+	while(!existMove && i < 8){
+		let j = 0;
+		while(!existMove && j < 8) {
+			if(gameBoard[i][j].currPiece != null){
+				if(gameBoard[i][j].currPiece.color === "black" && !gameBoard[i][j].currPiece.isKing){
+					let testX = 0;
+					let testY = 0;
+					if(i-1 >= 0) {
+						testX = i-1;
+						if(j-1 >= 0){
+							testY = j-1;
+							existMove = checkMove(testX, testY);
+						}
+					}
+					if(existMove){
+						console.log("first check success");
+						console.log("i: " + i);
+						console.log("j: " + j);
+						return existMove;
+					}
+					if(i - 1 >= 0) {
+						testX = i-1;
+						if(j+1 < 8) {
+							testY = j+1;
+							existMove = checkMove(testX, testY);
+						}
+					}
+					if(existMove){
+						console.log("i: " + i);
+						console.log("j: " + j);
+						console.log("second check success");
+						return existMove;
+					}
+					let jumpX = 0;
+					let jumpY = 0;
+					if(i-2 >= 0) {
+						testX = i-2;
+						jumpX = testX+1;
+						if(j-2 >= 0) {
+							testY = j-2;
+							jumpY = testY+1;
+							existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+						}
+					}
+					if(existMove){
+						console.log("tarX: " + testX);
+						console.log("tarY: " + testY);
+						return existMove;
+					}
+					if(i-2 >= 0){
+						testX = i-2;
+						jumpX = testX+1;
+						if(j+2 < 8){
+							testY = j+2;
+							jumpY = testY-1;
+							existMove = checkJump(gameBoard[i][j].currPiece.color, testX, testY, jumpX, jumpY);
+						}
+					}
+					if(existMove){
+						console.log("tarX " + testX);
+						console.log("tarY " + testY);
+						return existMove;
+					}
+				}
+			}
+			j++;
+		}
+		i++
+	}
+	console.log(existMove);
+	return existMove;
+}
+
+let checkMove = function(testX, testY) {
+	if(gameBoard[testX][testY].spaceColor === "black") {
+		if(gameBoard[testX][testY].isOpen === true){
+			return true;
+		}
+	}
+	return false;
+}
+let checkJump = function(pieceColor, targetX, targetY, jumpX, jumpY) {
+
+	let jumpColor = "";
+	if(pieceColor === "white"){
+		jumpColor = "black";
+	} else {
+		jumpColor = "white";
+	}
+	if(gameBoard[targetX][targetY].isOpen) {
+		if(gameBoard[jumpX][jumpY].currPiece != null) {
+			if(!gameBoard[jumpX][jumpY].isOpen && 
+				gameBoard[jumpX][jumpY].currPiece.color === jumpColor) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+let gameOverHandler = function(winnerColor) {
+	//console.log("here");
+	let domBoard = document.querySelector("#game-space");
+	domBoard.classList.add("do-not-show");
+	console.log(domBoard.classList);
+	let domGameOver = document.querySelector("#game-over");
+	domGameOver.classList.remove("do-not-show");
+	domGameOver.classList.add("show");
+	let winner = document.createElement("span");
+	let winnerString = "The winner is: " + winnerColor.toUpperCase();
+	winner.innerText = winnerString;
+	domGameOver.appendChild(winner);
+	let domUserInfoList = document.querySelectorAll(".user-info");
+	for(let i = 0; i < domUserInfoList.length; i++){
+		domUserInfoList[i].classList.add("do-not-show");
 	}
 }
 
@@ -310,4 +671,5 @@ let checkIfWhiteHasMove = function(){
 document.addEventListener("DOMContentLoaded", function() {
 	gameInit();
 	document.querySelector("#game-space").addEventListener("click", clickEventHandler);
+	//gameOverHandler("white");
 })
